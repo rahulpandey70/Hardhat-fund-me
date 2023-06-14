8e45fa4916908db09c7f400aefb747b44f87c8e2
rahulpandey70 <rahulpandey9103@gmail.com>
2023-06-14 14:55:52 +0530

    Update

--- hardhat.config.js
+++ hardhat.config.js
@@ -34,9 +34,11 @@ module.exports = {
         },
     },
     gasReporter: {
-        enabled: process.env.REPORT_GAS || undefined,
+        enabled: true,
+        outputFile: "gas-report.txt",
         noColors: true,
         currency: "USD",
+        token: "MATIC",
     },
     namedAccounts: {
         deployer: {
